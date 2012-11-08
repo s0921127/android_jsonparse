8b9bbccac7b911ff669ec965ed10f90f80c132d1
elysion <elysion@elysion-PC>
2012-11-08 18:30:30 +0900

    現在地のツリー表示

--- src/com/takanolab/android/JsonParse.java
+++ src/com/takanolab/android/JsonParse.java
@@ -17,6 +17,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ParseException;
 import android.net.Uri;
 import android.os.Bundle;
@@ -25,7 +30,7 @@ import android.view.ViewGroup.LayoutParams;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-public class JsonParse extends Activity {
+public class JsonParse extends Activity implements LocationListener {
 	static final String SEARCH_URL = "https://api.foursquare.com/v2/venues/search?ll=35.6865271,139.692311&" +
 			"oauth_token=G4RUWUY4YAKSUWLEWLXPHRBQJZATY4XPTIAI4OT02CXMLMM3&v=20121017";
 	
@@ -35,11 +40,42 @@ public class JsonParse extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        // LocationManagerを取得
+        LocationManager mLocationManager =
+             (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+ 
+        // Criteriaオブジェクトを生成
+        Criteria criteria = new Criteria();
+ 
+        // Accuracyを指定(低精度)
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+         
+        // PowerRequirementを指定(低消費電力)
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+         
+        // ロケーションプロバイダの取得
+        String provider = mLocationManager.getBestProvider(criteria, true);
+ 
+        // LocationListenerを登録
+        mLocationManager.requestLocationUpdates(provider, 0, 0, this);
+        
+    }
+    
+    @Override
+    public void onLocationChanged(Location location) {
+        // 緯度の表示
+        //TextView tv_lat = (TextView) findViewById(R.id.Latitude);
+        //tv_lat.setText("Latitude:"+location.getLatitude());
+ 
+        // 経度の表示
+        //TextView tv_lng = (TextView) findViewById(R.id.Longitude);
+        //tv_lng.setText("Longitude:"+location.getLongitude());
+        
         String scheme = "https";
         String authority = "api.foursquare.com";
         String path = "/v2/venues/search";
         
-        String ll = "35.485356, 139.341346";
+        String ll = location.getLatitude() + ", " + location.getLongitude();
         String oauth_token = "G4RUWUY4YAKSUWLEWLXPHRBQJZATY4XPTIAI4OT02CXMLMM3";
         String v = "20121017";
         Uri.Builder uriBuilder = new Uri.Builder();
@@ -52,7 +88,6 @@ public class JsonParse extends Activity {
         uriBuilder.appendQueryParameter("v", v);
      
         String uri = uriBuilder.toString();
-        Log.d("test",uri);
         
         HttpClient httpClient = new DefaultHttpClient();
         HttpParams params = httpClient.getParams();
@@ -117,5 +152,24 @@ public class JsonParse extends Activity {
         ScrollView scrollView = new ScrollView(this);
         scrollView.addView(textView);
         setContentView(scrollView, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+ 
+    }
+ 
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+         
+    }
+ 
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+         
+    }
+ 
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+ 
     }
 }
